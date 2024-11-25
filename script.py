27c75b7168555e0985abf04829c0ad9c4e85a31d
Tee6 <n.leimegger@gmail.com>
2024-11-25 20:31:55 +0100

    added working hours calculation

--- script.py
+++ script.py
@@ -55,8 +55,32 @@ service = authenticate_google_account()
 
 # Arbeitszeiten Definieren nach Dienstplan
 Jahr = 2024
-Monat = 11
-kalender_map = {
+Monat = 12
+
+kalender_map_okt = { # Oktober 2024
+    "ZT": {
+        'days': [1,2,3,4,15,16,17,18,21,22,23,24,25,28,29,30], # Wochentage an denen ich Tagdienst habe
+        'start_time': [7, 30], 
+        'end_time': [16, 30]
+    },
+    "Z2": {
+        'days': [5], # WochenendTage, an denen ich Tagdienst habe
+        'start_time': [8, 0], 
+        'end_time': [14, 0],
+    },
+    "Z3": {
+        'days': [9,10,11], # Wochentage an denen ich Spätdienst habe
+        'start_time': [15, 30], 
+        'end_time': [23, 0],
+    },
+    "Z4": {
+        'days': [12,13], # Wochenendtage an denen ich Spätdienst habe
+        'start_time': [18, 0], 
+        'end_time': [23, 0],
+    },
+}
+
+kalender_map_nov = { # November 2024
     "ZT": {
         'days': [5,6,7,8,11,12,21,22,25,26,27,28], # Wochentage an denen ich Tagdienst habe
         'start_time': [7, 30], 
@@ -79,6 +103,50 @@ kalender_map = {
     },
 }
 
-# Events erstellen
-for key, data in kalender_map.items():
-    create_calendar_events(service, Jahr, Monat, key, data['days'], data['start_time'], data['end_time'])
+kalender_map_dez = { # Dezember 2024
+    "ZT": {
+        'days': [4,5,6,10,11,12,13,30,31], # Wochentage an denen ich Tagdienst habe
+        'start_time': [7, 30], 
+        'end_time': [16, 30]
+    },
+    "Z2": {
+        'days': [14], # WochenendTage, an denen ich Tagdienst habe
+        'start_time': [8, 0], 
+        'end_time': [14, 0],
+    },
+    "Z3": {
+        'days': [16,17,18,19,20,27], # Wochentage an denen ich Spätdienst habe
+        'start_time': [15, 30], 
+        'end_time': [23, 0],
+    },
+    "Z4": {
+        'days': [1,7,8,23,24,25,26], # Wochenendtage an denen ich Spätdienst habe
+        'start_time': [18, 0], 
+        'end_time': [23, 0],
+    },
+}
+
+kalender_map = kalender_map_dez
+
+input = input("Events erstellen (e) oder Arbeitszeit berechnen (b)?")
+Tagesstunden = 40/7
+if input == "b":
+    # Arbeitszeiten berechnen
+    total_hours = 0
+    for key, data in kalender_map.items():
+        total_shift_hours = 0
+        for day in data['days']:
+            start_time = datetime.datetime(Jahr, Monat, day, data['start_time'][0], data['start_time'][1])
+            end_time = datetime.datetime(Jahr, Monat, day, data['end_time'][0], data['end_time'][1])
+            diff = end_time - start_time
+            if key == "ZT":
+                diff -= datetime.timedelta(hours=0.5)  # Pause abziehen
+            total_shift_hours += diff.total_seconds() / 3600
+        print(f"Arbeitszeit für {key}: {total_shift_hours} Stunden")
+        total_hours += total_shift_hours
+    print(f"Gesamte Arbeitszeit: {total_hours} Stunden. Sollwert: ", round(Tagesstunden*31))
+    print(f"Überstunden: {round(total_hours - Tagesstunden*31)} Stunden")
+if input == "e":
+    # Events erstellen
+    for key, data in kalender_map.items():
+        create_calendar_events(service, Jahr, Monat, key, data['days'], data['start_time'], data['end_time'])
